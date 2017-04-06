7e2a85c257a025e4d7d6dd2be2472384cb49dabf
lbosch <lbosch@icra.cat>
2017-04-06 15:26:04 +0100

    canvis

--- test.py
+++ test.py
@@ -1,84 +1,84 @@
 # -*- coding: utf-8 -*-
 '''
-Script per fer una prova de mínims:
-    1. Crear una connexió serial
-    2. Enviar una trama
-    3. Rebre una resposta
+    Script per fer una prova de mínims:
+        1. Crear una connexió serial
+        2. Enviar una trama
+        3. Rebre una resposta
 
-Comandes AT per testing (manual Siretta)
-========================================
-    "descripcio"
-        > "Comanda"
-        < "Respostes possibles"
+    Comandes AT per testing (manual Siretta)
+    ========================================
+        "descripcio"
+            > "Comanda"
+            < "Respostes possibles"
 
-    check serial port
-        > AT
-        < OK
+        check serial port
+            > AT
+            < OK
 
-    set verbose error reporting
-        > AT+CMEE=2
-        < OK
+        set verbose error reporting
+            > AT+CMEE=2
+            < OK
 
-    get parameters
-        > AT&V
-        < [...] OK
+        get parameters
+            > AT&V
+            < [...] OK
 
-    check pin registration
-        > AT+CPIN?
-        < +CPIN: READY
-        < OK
-        < +CME ERROR: SIM not inserted
-        < +CME ERROR: SIM pin
+        check pin registration
+            > AT+CPIN?
+            < +CPIN: READY
+            < OK
+            < +CME ERROR: SIM not inserted
+            < +CME ERROR: SIM pin
 
-    query firmware version
-        > AT+GMR
-        < Firmware version
+        query firmware version
+            > AT+GMR
+            < Firmware version
 
-    query imei number
-        > AT+CGSN
-        < IMEI number
+        query imei number
+            > AT+CGSN
+            < IMEI number
 
-    set network registration to 'ALL'
-        > AT+COPS=0
-        < OK
+        set network registration to 'ALL'
+            > AT+COPS=0
+            < OK
 
-    check network registration
-        > AT+CREG?
-        < +CREG:0,1 (registered to local network)
-        < +CREG:0,5 (registered to roaming network)
-        < +CREG:0,0 (not registered to a network)
-        < +CREG:0,2 (searching for a network)
-        < +CREG:0,3 (registration is denied)
+        check network registration
+            > AT+CREG?
+            < +CREG:0,1 (registered to local network)
+            < +CREG:0,5 (registered to roaming network)
+            < +CREG:0,0 (not registered to a network)
+            < +CREG:0,2 (searching for a network)
+            < +CREG:0,3 (registration is denied)
 
-    check network signal strength
-        > AT+CSQ
-        < +CSQ: 23,1 (value>9 if successful)
-        < +CSQ: 99,1 (value=99 if no signal)
-        < +CSQ: 3,1 (value<9 if ther are network connectivity issues)
+        check network signal strength
+            > AT+CSQ
+            < +CSQ: 23,1 (value>9 if successful)
+            < +CSQ: 99,1 (value=99 if no signal)
+            < +CSQ: 3,1 (value<9 if ther are network connectivity issues)
 
-    check GPRS context availability
-        > AT+CGATT?
-        < +CGATT:1 (if successful)
-        < OK
+        check GPRS context availability
+            > AT+CGATT?
+            < +CGATT:1 (if successful)
+            < OK
 
-    set GPRS APN for your network
-        >AT+CGDCONT=1,"IP","APN name"
-        < OK
+        set GPRS APN for your network
+            >AT+CGDCONT=1,"IP","APN name"
+            < OK
 
-    connect to GPRS context
-        > AT#SGACT=1,1,"APN username","APN password"
-        < #SGACT:x.x.x.x
-        < OK
-        < ERROR
+        connect to GPRS context
+            > AT#SGACT=1,1,"APN username","APN password"
+            < #SGACT:x.x.x.x
+            < OK
+            < ERROR
 
-    setup tcp socket connection parameters
-        > AT#SCFG:1,1,300,90,600,50
-        < OK
+        setup tcp socket connection parameters
+            > AT#SCFG:1,1,300,90,600,50
+            < OK
 
-    connect TCP socket to server ip address
-        > AT#SD=1,0,xxxx,"y.y.y.y"
-        < CONNECT
-        < NO CARRIER
+        connect TCP socket to server ip address
+            > AT#SD=1,0,xxxx,"y.y.y.y"
+            < CONNECT
+            < NO CARRIER
 '''
 import serial
 print "+------------------------------+"
@@ -87,7 +87,7 @@ print "+------------------------------+"
 
 #obre serial
 ser=serial.Serial()
-ser.port="/dev/ttyS5"
+ser.port="/dev/ttyS17"
 ser.baudrate=9600
 ser.bytesize=8
 ser.parity=serial.PARITY_EVEN
